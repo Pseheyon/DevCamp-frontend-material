9bae8dd4bad2782072e1d5c55b32e0f21eca5940
sehyeon <boksil211@naver.com>
2024-03-21 02:26:09 +0900

    [fix] 다크모드 Extra attributes.. 에러
    원인
    server에서 첫 렌더링 시 theme는 서버에서 알 수 없는 상태이기 때문에 undefined의 상태이고 ThemeProvider가 렌더가 되면 theme의 상태가 존재하므로 server와 client의 구조가 달라지게 되어 hydration 에러가 발생하는 것이었다.
    
    해결 방법
    Providers 컴포넌트에 현재 마운트 상태를 확인하는 상태 isMount를 false로 초기화하고 isMount가 false 일 때는 null을 리턴하고, true 일 때는 ThemeProvider를 리턴하도록 작성하면 해당 에러가 사라지는 것을 확인할 수 있다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { DarkModeToggle } from "@/components/dark-mode-toggle";
--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,7 +1,20 @@
 "use client";
+import { useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isMount, setMount] = useState(false);
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
+  if (!isMount) {
+    return null;
+  }
+
+  return <NextThemesProvider attribute="class">{children}</NextThemesProvider>;
+};
+
+export default ThemeProvider;

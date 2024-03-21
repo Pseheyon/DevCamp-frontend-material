3e109544729c352330181c4c6ca5e67f1698b29a
sehyeon <boksil211@naver.com>
2024-03-21 18:38:08 +0900

    [fix] ThemeProviderProps 설정
    루트레이아웃에  attribute 속성을 지원하지 않는 구성 요소나 요소에 속성 attribute를 할당하려고 시도한 것의 문제가 생겼었다.
     <ThemeProvider
              attribute="class"
              defaultTheme="system"
              enableSystem
              disableTransitionOnChange
    ThemeProvider 에 잘 못 설정한게 있는 것이다.
    에러가 생겼던 이유는 next-themes 기능인 ThemeProviderProps이 안되어 있었기 때문이다. 다시 제대로 설정하니
    const [isMount, setMount] = useState(false);
    
      useEffect(() => {
        setMount(true);
      }, []);
    
      if (!isMount) {
        return null;
      }
      위의 코드가 src/components/theme-provider.tsx 에 없어도 잘 작동하는 모습을 볼 수 있다.

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,18 +3,6 @@ import { useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
-const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isMount, setMount] = useState(false);
-
-  useEffect(() => {
-    setMount(true);
-  }, []);
-
-  if (!isMount) {
-    return null;
-  }
-
-  return <NextThemesProvider attribute="class">{children}</NextThemesProvider>;
-};
-
-export default ThemeProvider;
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}

af62857772b31d36b92a5f7962c717c2319ef052
sehyeon <boksil211@naver.com>
2024-03-28 00:33:20 +0900

    [fix] 라우트 파일 형식 맞추기
     API 라우트 파일에서 사용되는 데이터이므로 라우트 파일 밖에서 정의하고 해당 파일에서 가져와 사용

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -2,45 +2,7 @@ import { NextResponse } from "next/server";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { TsOrderSchemaType, orderSchema } from "@/validators/cartSchema";
-
-export const mockCartData: TsOrderSchemaType[] = [
-  {
-    user: {
-      username: "루미",
-      email: "lumi@example.com",
-      phoneNumber: "123-456-7890",
-    },
-    productInfo: {
-      productname: "루미고양이",
-      productdetail: "너무 귀엽지",
-      price: 1000,
-      quantity: 2,
-    },
-    shippingInfo: {
-      address: "123 부산 중구 대동로 40-21번지 길바닥집, ",
-      shippingType: "dor",
-      recipient: "쫀덕이",
-      recipientphone: "123-456-7890",
-    },
-    coupon: {
-      couponPoint: 6000,
-      couponCode: "DISCOUNT123-20% 할인 쿠폰",
-      pointsUsed: 6000,
-    },
-    paymentAmount: {
-      discount: 0,
-      total: 2000,
-    },
-    paymentMethod: {
-      payment: "",
-      depositor: "박루미",
-    },
-    purchaseAgreement: {
-      termsAndConditions: false,
-      privacyPolicy: false,
-    },
-  },
-];
+import { mockCartData } from "@/app/data/mockCartData";
 
 export async function POST(request: Request) {
   const body: unknown = await request.json();
---
+++ src/app/data/mockCartData.ts
@@ -0,0 +1,40 @@
+import { TsOrderSchemaType, orderSchema } from "@/validators/cartSchema";
+
+export const mockCartData: TsOrderSchemaType[] = [
+  {
+    user: {
+      username: "루미",
+      email: "lumi@example.com",
+      phoneNumber: "123-456-7890",
+    },
+    productInfo: {
+      productname: "루미고양이",
+      productdetail: "너무 귀엽지",
+      price: 1000,
+      quantity: 2,
+    },
+    shippingInfo: {
+      address: "123 부산 중구 대동로 40-21번지 길바닥집, ",
+      shippingType: "dor",
+      recipient: "쫀덕이",
+      recipientphone: "123-456-7890",
+    },
+    coupon: {
+      couponPoint: 6000,
+      couponCode: "DISCOUNT123-20% 할인 쿠폰",
+      pointsUsed: 6000,
+    },
+    paymentAmount: {
+      discount: 0,
+      total: 2000,
+    },
+    paymentMethod: {
+      payment: "",
+      depositor: "박루미",
+    },
+    purchaseAgreement: {
+      termsAndConditions: false,
+      privacyPolicy: false,
+    },
+  },
+];

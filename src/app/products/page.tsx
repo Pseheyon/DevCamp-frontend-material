9f083a34a7507b1d52b89d8fffb4b7287282a085
sehyeon <boksil211@naver.com>
2024-03-28 21:41:06 +0900

    [fix] 상품리스트 필요정보만 뽑음

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,48 +4,47 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { GetStaticProps } from "next";
+import { getItems } from "../../../pages/api/products";
 
-export default function List() {
-  const [products, setProducts] = useState<
-    { id: string; properties: { id: string }[] }[]
-  >([]);
-  // 📌 useEffect로 페이지 로딩과 동시에 products api 데이터를 받아와서 set설정
-  useEffect(() => {
-    fetch(`http://localhost:3000/api/products`)
-      .then((res) => res.json())
-      .then((data) => setProducts(data.items));
-  }, []);
-
+export const getStaticProps: GetStaticProps = async () => {
+  const list = await getItems();
+  return {
+    props: {
+      list,
+    },
+  };
+};
+interface List {
+  id: string;
+  productName: string;
+  description: string;
+  price: string[];
+}
+interface Props {
+  list: List[];
+}
+export default function List({ list }: Props) {
   return (
-    <div>
-      <p>Product List</p>
-      // 📌 products 내용 화면에 표시
-      {products &&
-        products?.map((item) => (
-          <div key={item.id}>
-            {JSON.stringify(item)}
-            // 📌 item property들을 버튼으로 화면에 표시
-            {item.properties &&
-              Object.entries(item.properties).map(([key, value]) => (
-                // 📌 버튼 클릭 시 detail api 호출 해서 alert로 내용 표시
-                <button
-                  key={key}
-                  onClick={() => {
-                    fetch(
-                      `http://localhost:3000/api/detail?pageId=${item.id}&propertyId=${value.id}`
-                    )
-                      .then((res) => res.json())
-                      .then((data) => alert(JSON.stringify(data.detail)));
-                  }}
-                >
-                  {key}
-                </button>
-              ))}
-            <br />
-            <br />
-          </div>
-        ))}
-    </div>
+    <main>
+      {/* <Button onClick={handleClick}>add jacket 2</Button> */}
+      <div>
+        <p>Product List</p>
+        <div>
+          {list.map((list) => (
+            <div key={list.id}>
+              <h2>{list.productName}</h2>
+              <ul>
+                {list.price.map((tag) => (
+                  <li key={tag}>{tag}</li>
+                ))}
+              </ul>
+              <p>{list.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }

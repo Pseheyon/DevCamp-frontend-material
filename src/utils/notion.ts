f8dc98dbb8276679aa238536c87d0246c2f1165a
sehyeon <boksil211@naver.com>
2024-07-19 20:36:47 +0900

    [test]env안됨

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -13,21 +13,19 @@ import { TProductPage, TProduct } from "@/interfaces/ProductIF";
 const notionSecret = process.env.NOTION_TOKEN;
 const notionDatabaseId = process.env.NOTION_DATABASE_ID;
 
-export const notion = new Client({
-  auth: "secret_zFWPNbQisqv4qVb59qPMVROEusPjRYcl0RKcgT1xChm",
-});
+export const notion = new Client({ auth: notionSecret });
 
 const databaseId = process.env.NOTION_DATABASE_ID!;
 
 const queryNotionDatabase = async (): Promise<TProductPage[]> => {
   try {
     const response = await notion.databases.query({
-      filter: {
-        property: "status",
-        select: {
-          equals: "published",
-        },
-      },
+      // filter: {
+      //   property: "status",
+      //   select: {
+      //     equals: "published",
+      //   },
+      // },
       database_id: databaseId,
     });
     //console.log("응답----------------------", response);
